403ef10ad5aac8b138728acefaca92d65c7feecf
hjf <2993107725@qq.com>
2024-04-29 20:01:39 +0800

    springboot_09_ssm third commit

--- src/main/java/com/itjiangfeng/service/impl/BookServiceImpl.java
+++ src/main/java/com/itjiangfeng/service/impl/BookServiceImpl.java
@@ -41,7 +41,7 @@ public class BookServiceImpl implements BookService {
     @Override
     public Book getById(Integer id) {
         //模拟业务异常，包装成自定义异常
-        if(id == 1){
+        /*if(id == 1){
            throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
         }
         //模拟系统异常，将可能出现的异常进行包装，转换成自定义异常
@@ -49,7 +49,7 @@ public class BookServiceImpl implements BookService {
             int i = 1/0;
         }catch (Exception e){
             throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试!",e);
-        }
+        }*/
         return bookDao.getById(id);
     }
 
