3dadbbfc1fadd98d2955141aa1124b89d7405fd7
hjf <2993107725@qq.com>
2024-04-30 20:04:59 +0800

    push commit

--- src/main/java/com/itjiangfeng/service/impl/BookServiceImpl.java
+++ src/main/java/com/itjiangfeng/service/impl/BookServiceImpl.java
@@ -44,8 +44,11 @@ public class BookServiceImpl implements BookService {
         /*if(id == 1){
            throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
         }
+         if(id == 1){
+            throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
+        }
         //模拟系统异常，将可能出现的异常进行包装，转换成自定义异常
-        try{
+        /*try{
             int i = 1/0;
         }catch (Exception e){
             throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试!",e);
